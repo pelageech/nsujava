e9f49ee178c59a00edd5b49bad8129490fe98476
pelageech <va.oleg2015@yandex.ru>
2022-09-13 15:57:19 +0700

    ALLELUIA

---
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class Heapsort {
+
+    public static List<Integer> heapsort(List<Integer> arr) {
+        int arr_size = arr.size();
+
+        for (int i = arr_size - 1; i >= 0; i--) {
+            heapify(arr, arr_size, i);
+        }
+
+        for (int i = arr_size - 1; i > 0; i--) {
+            Collections.swap(arr, i, 0);
+            heapify(arr, i, 0);
+        }
+
+        return arr;
+    }
+
+    private static void heapify(List<Integer> arr, int n, int i) {
+        int large = i;
+        int l = 2 * i + 1;
+        int r = l + 1;
+
+        if (l < n && arr.get(l) > arr.get(large)) {
+            large = l;
+        }
+        if (r < n && arr.get(r) > arr.get(large)) {
+            large = r;
+        }
+
+        if (i != large) {
+            Collections.swap(arr, i, large);
+            heapify(arr, n, large);
+        }
+    }
+
+}
---
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class HeapsortTest {
+    @Test
+
+    public void TheFirstTest() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> actual = Heapsort.heapsort(Arrays.asList(3, 2, 5, 1, 4));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void SortedArrayTest() {
+        List<Integer> expected = Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010);
+
+        List<Integer> actual = Heapsort.heapsort(Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void RSortedArrayTest() {
+        List<Integer> expected = Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010);
+
+        List<Integer> actual = Heapsort.heapsort(Arrays.asList(1010, 1000, 900, 80, 19, 10, 10, 6, 3, 1));
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
