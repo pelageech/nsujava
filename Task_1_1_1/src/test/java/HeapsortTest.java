a006ba9b9c34242d6672aaa910a0a5a080996fa2
pelageech <va.oleg2015@yandex.ru>
2022-09-14 13:31:36 +0700

    deleted:    Task_1_1_1/.gradle/7.4/checksums/checksums.lock 	deleted:    Task_1_1_1/.gradle/7.4/dependencies-accessors/dependencies-accessors.lock 	deleted:    Task_1_1_1/.gradle/7.4/dependencies-accessors/gc.properties 	deleted:    Task_1_1_1/.gradle/7.4/executionHistory/executionHistory.bin 	deleted:    Task_1_1_1/.gradle/7.4/executionHistory/executionHistory.lock 	deleted:    Task_1_1_1/.gradle/7.4/fileChanges/last-build.bin 	deleted:    Task_1_1_1/.gradle/7.4/fileHashes/fileHashes.bin 	deleted:    Task_1_1_1/.gradle/7.4/fileHashes/fileHashes.lock 	deleted:    Task_1_1_1/.gradle/7.4/gc.properties 	deleted:    Task_1_1_1/.gradle/buildOutputCleanup/buildOutputCleanup.lock 	deleted:    Task_1_1_1/.gradle/buildOutputCleanup/cache.properties 	deleted:    Task_1_1_1/.gradle/buildOutputCleanup/outputFiles.bin 	deleted:    Task_1_1_1/.gradle/file-system.probe 	deleted:    Task_1_1_1/.gradle/vcs-1/gc.properties 	modified:   Task_1_1_1/src/test/java/HeapsortTest.java

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class HeapsortTest {
     @Test
 
-    public void TheFirstTest() {
+    public void theFirstTest() {
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
 
         List<Integer> actual = Heapsort.heapsort(Arrays.asList(3, 2, 5, 1, 4));
@@ -18,7 +18,7 @@ public class HeapsortTest {
 
     @Test
 
-    public void SortedArrayTest() {
+    public void sortedArrayTest() {
         List<Integer> expected = Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010);
 
         List<Integer> actual = Heapsort.heapsort(Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010));
@@ -28,7 +28,7 @@ public class HeapsortTest {
 
     @Test
 
-    public void RSortedArrayTest() {
+    public void rSortedArrayTest() {
         List<Integer> expected = Arrays.asList(1, 3, 6, 10, 10, 19, 80, 900, 1000, 1010);
 
         List<Integer> actual = Heapsort.heapsort(Arrays.asList(1010, 1000, 900, 80, 19, 10, 10, 6, 3, 1));
