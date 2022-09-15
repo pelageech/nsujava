6c7276de87a61ca1c27755b901115ba3f8ec619c
pelageech <va.oleg2015@yandex.ru>
2022-09-15 09:32:00 +0700

    corrected style #1

--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -1,24 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.util.Collections;
+import java.util.List;
 
+/**
+ * Heapsort is a O(NlogN)-sort based on array (heap).
+ * Array becomes a binary heap:
+ *
+ *          0 (item)
+ *         /        \
+ *      1 (item)   2 (item)
+ *      /      \
+ *   3 (item)  4 (item)
+ *
+ * and so on.
+ */
 public class Heapsort {
 
+    /**
+     * @param arr start Array
+     * @return Sorted Array
+     */
     public static List<Integer> heapsort(List<Integer> arr) {
-        int arr_size = arr.size();
+        int arrsize = arr.size();
 
-        for (int i = arr_size - 1; i >= 0; i--) {
-            heapify(arr, arr_size, i);
+        for (int i = arrsize - 1; i >= 0; i--) {
+            heapify(arr, arrsize, i);
         }
 
-        for (int i = arr_size - 1; i > 0; i--) {
+        for (int i = arrsize - 1; i > 0; i--) {
             Collections.swap(arr, i, 0);
             heapify(arr, i, 0);
         }
         return arr;
     }
 
+    /**
+     * @param arr Array (binary heap)
+     * @param n Conditional size of arr
+     * @param i Root of (sub-)tree
+     */
     private static void heapify(List<Integer> arr, int n, int i) {
         int large = i;
         int l = 2 * i + 1;
