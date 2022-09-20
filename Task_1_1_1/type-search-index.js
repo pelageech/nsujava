c6a5a97abc369416c7d4b83572678bd7bff7b8f7
pelageech <pelageech@users.noreply.github.com>
2022-09-20 13:33:51 +0000

    Deploying to gh-pages from @ pelageech/nsujava@4039ba5e338051846fe0206bf02a88bac970bdda 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Heapsort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.ablaginin","l":"Heapsort"}]
