158361736e92715c931331af5d0fbf21d840a037
pelageech <va.oleg2015@yandex.ru>
2022-09-21 17:16:05 +0700

    added executing script

--- Task_1_1_1/script.bat
+++ Task_1_1_1/script.bat
@@ -4,3 +4,5 @@ javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\ablagini
 
 mkdir .\make\jar
 jar cf .\make\jar\Heapsort.jar -C .\make\bin .
+
+java -classpath .\make\jar\Heapsort.jar ru.nsu.ablaginin.Heapsort
