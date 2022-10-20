613179f9b568fc45c68869d4de8a0de2e0745b05
pelageech <va.oleg2015@yandex.ru>
2022-10-20 17:11:45 +0700

    Unit Tests

--- Task_1_2_3/src/test/java/ru/nsu/ablaginin/GraphTest.java
+++ Task_1_2_3/src/test/java/ru/nsu/ablaginin/GraphTest.java
@@ -11,26 +11,136 @@ class GraphTest {
 
   @Test
   void addVertex() {
+
+    // actual
+    Graph<String> graph = new Graph<>();
+    boolean t = graph.addVertex("one");
+    boolean f = graph.addVertex("one");
+
+    // expected
+    Graph<String> expected = new Graph<>();
+    expected.addVertex("one");
+
+    // asserts
+    assertEquals(expected, graph);
+    assertTrue(t);
+    assertFalse(f);
   }
 
   @Test
-  void addAll() {
+  void addAllVertexes() {
+
+    // actual
+    Graph<String> graph = new Graph<>();
+    int count = graph.addAllVertexes(new String[]{"one", "two", "three", "one"});
+
+    // expected
+    Graph<String> expected = new Graph<>();
+    expected.addAllVertexes(new String[]{"one", "two", "three"});
+
+    // asserts
+    assertEquals(expected, graph);
+    assertEquals(3, count);
+  }
+
+  @Test
+  void deleteVertex() {
+
+    // actual
+    Graph<String> graph = new Graph<>();
+    graph.addAllVertexes(new String[]{"one", "two", "three", "four"});
+    boolean t = graph.deleteVertex("three");
+    boolean f = graph.deleteVertex("three");
+
+    // expected
+    Graph<String> expected = new Graph<>();
+    expected.addAllVertexes(new String[]{"one", "two", "four"});
+
+    // asserts
+    assertEquals(expected, graph);
+    assertTrue(t);
+    assertFalse(f);
   }
 
   @Test
   void addEdge() {
+
+    // actual
+    Graph<String> graph = new Graph<>();
+    graph.addAllVertexes(new String[]{"one", "two", "three"});
+
+    graph.addEdge("one", "two", 3);
+    graph.addEdge("one", "three", 1);
+    boolean t = graph.addEdge("three", "two", 5);
+    boolean f = graph.addEdge("two", "three", -3);
+
+    // expected
+    Graph<String> expected = new Graph<>();
+    expected.addAllVertexes(new String[]{"one", "two", "three"});
+    expected.addEdge("one", "two", 3);
+    expected.addEdge("one", "three", 1);
+    expected.addEdge("three", "two", 5);
+
+    // asserts
+    assertEquals(expected, graph);
+    assertTrue(t);
+    assertFalse(f);
   }
 
   @Test
-  void testAddEdge() {
+  void deleteEdge() {
+    // actual
+    Graph<String> graph = new Graph<>();
+    graph.addAllVertexes(new String[]{"one", "two", "three"});
+
+    graph.addEdge("one", "two", 3);
+    graph.addEdge("one", "three", 1);
+    graph.addEdge("three", "two", 5);
+    graph.addEdge("two", "three", 3);
+    boolean t = graph.deleteEdge("two", "three");
+    boolean f = graph.deleteEdge("two", "three");
+
+    // expected
+    Graph<String> expected = new Graph<>();
+    expected.addAllVertexes(new String[]{"one", "two", "three"});
+    expected.addEdge("one", "two", 3);
+    expected.addEdge("one", "three", 1);
+    expected.addEdge("three", "two", 5);
+
+    // asserts
+    assertEquals(expected, graph);
+    assertTrue(t);
+    assertFalse(f);
   }
 
   @Test
   void dijkstra() {
-  }
 
-  @Test
-  void testDijkstra() {
+    // actual
+    Graph<String> graph = new Graph<>();
+    graph.addAllVertexes(new String[]{"one", "two", "three", "four"});
+    graph.addEdge("one", "two", 3);
+    graph.addEdge("one", "three", 10);
+    graph.addEdge("two", "three", 1);
+    graph.addEdge("two", "four", 4);
+    graph.addEdge("three", "four", 5);
+
+    List<Vertex<String>> vertexList = graph.dijkstra("one");
+    String[] strActual = new String[4];
+    Integer[] weightActual = new Integer[4];
+
+    for (int i = 0; i < 4; i++) {
+      strActual[i] = vertexList.get(i).getKey();
+      weightActual[i] = vertexList.get(i).getValue();
+    }
+
+    // expected
+    String[] strExpected = new String[]{"one", "two", "three", "four"};
+    Integer[] weightsExpected = new Integer[]{0, 3, 4, 7};
+
+    // asserts
+    assertArrayEquals(strExpected, strActual);
+    assertArrayEquals(weightsExpected, weightActual);
   }
 
   @Test
