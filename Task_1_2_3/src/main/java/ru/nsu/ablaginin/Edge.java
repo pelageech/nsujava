f22f0330daf523fa6d14bf8e271ffa5b5b20fc95
pelageech <va.oleg2015@yandex.ru>
2022-10-24 19:40:57 +0700

    Edge rewritten to Record

--- Task_1_2_3/src/main/java/ru/nsu/ablaginin/Edge.java
+++ Task_1_2_3/src/main/java/ru/nsu/ablaginin/Edge.java
@@ -7,23 +7,7 @@ import java.util.Objects;
  *
  * @param <T> type of vertex's name, must be Comparable
  */
-public class Edge<T extends Comparable<T>> {
-  private final Vertex<T> from;
-  private final Vertex<T> to;
-  private final Integer weight;
-
-  /**
-   * Implements one edge.
-   *
-   * @param v1 from-vertex
-   * @param v2 to-vertex
-   * @param w weight of the edge
-   */
-  public Edge(Vertex<T> v1, Vertex<T> v2, Integer w) {
-    from = v1;
-    to = v2;
-    weight = w;
-  }
+record Edge<T extends Comparable<T>> (Vertex<T> from, Vertex<T> to, Integer weight) {
 
   public Integer getWeight() {
     return weight;
