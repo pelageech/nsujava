eaad70291cf66ff95527003bcc2b2de7f62749b2
pelageech <va.oleg2015@yandex.ru>
2022-11-16 14:06:07 +0700

    try-with-resources in tests largeTest improved

--- Task_1_3_1/src/test/java/ru/nsu/ablaginin/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/ablaginin/SubstringTest.java
@@ -9,7 +9,9 @@ import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.nio.channels.Channels;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
@@ -18,76 +20,71 @@ class SubstringTest {
   public void commonTest() {
 
     // actual
-    Scanner isc = null;
-    InputStream fin = getClass().getClassLoader().getResourceAsStream("f1.in");
-    if (fin != null) {
-      isc = new Scanner(fin);
-    } else {
+    try (InputStream fin = getClass().getClassLoader().getResourceAsStream("f1.in");
+         Scanner isc = new Scanner(Objects.requireNonNull(fin))
+    ){
+      String textInputString = isc.nextLine();
+      InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
+      if (textStream == null) {
+        fail();
+      }
+
+      Substring fsub = new Substring(textStream);
+
+      List<LinePointers> actual;
+      String find = isc.nextLine();
+
+      actual = fsub.algorithmRabinKarp(find);
+
+      // expected
+      List<LinePointers> expected = new ArrayList<>();
+      expected.add(new LinePointers(1, List.of(0)));
+      expected.add(new LinePointers(2, List.of(2)));
+      expected.add(new LinePointers(3, List.of(2)));
+      expected.add(new LinePointers(4, List.of(7, 24)));
+      expected.add(new LinePointers(5, List.of(1)));
+
+      // asserts
+      assertEquals(expected, actual);
+    } catch (Exception e) {
       fail();
     }
-
-    String textInputString = isc.nextLine();
-    InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
-    if (textStream == null) {
-      fail();
-    }
-
-    Substring fsub = new Substring(textStream);
-
-    List<LinePointers> actual;
-    String find = isc.nextLine();
-
-    actual = fsub.algorithmRabinKarp(find);
-
-    // expected
-    List<LinePointers> expected = new ArrayList<>();
-    expected.add(new LinePointers(1, List.of(0)));
-    expected.add(new LinePointers(2, List.of(2)));
-    expected.add(new LinePointers(3, List.of(2)));
-    expected.add(new LinePointers(4, List.of(7, 24)));
-    expected.add(new LinePointers(5, List.of(1)));
-
-    // asserts
-    assertEquals(expected, actual);
   }
 
   @Test
   public void warAndPeaceTest() {
     // actual
-    InputStream fin = getClass().getClassLoader().getResourceAsStream("f2.in");
-    if (fin != null) {
-      try (Scanner isc = new Scanner(fin)) {
-        String textInputString = isc.nextLine();
-        InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
-        if (textStream == null) {
-          fail();
-        }
-
-        Substring fsub = new Substring(textStream);
-
-        List<LinePointers> actual;
-        String find = isc.nextLine();
-
-        actual = fsub.algorithmRabinKarp(find);
-
-        // expected
-        List<LinePointers> expected = new ArrayList<>();
-        expected.add(new LinePointers(2967, List.of(37)));
-        expected.add(new LinePointers(3258, List.of(158)));
-        expected.add(new LinePointers(3323, List.of(25, 38)));
-        expected.add(new LinePointers(4058, List.of(687)));
-        expected.add(new LinePointers(12310, List.of(67)));
-        expected.add(new LinePointers(12655, List.of(1033)));
-        expected.add(new LinePointers(12772, List.of(507)));
-        expected.add(new LinePointers(13749, List.of(1459)));
-        expected.add(new LinePointers(16153, List.of(48, 216)));
-
-        // asserts
-        assertEquals(expected, actual);
-      } catch (Exception e) {
+    try (InputStream fin = getClass().getClassLoader().getResourceAsStream("f2.in");
+         Scanner isc = new Scanner(Objects.requireNonNull(fin))
+    ) {
+      String textInputString = isc.nextLine();
+      InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
+      if (textStream == null) {
         fail();
       }
-    } else {
+
+      Substring fsub = new Substring(textStream);
+
+      List<LinePointers> actual;
+      String find = isc.nextLine();
+
+      actual = fsub.algorithmRabinKarp(find);
+
+      // expected
+      List<LinePointers> expected = new ArrayList<>();
+      expected.add(new LinePointers(2967, List.of(37)));
+      expected.add(new LinePointers(3258, List.of(158)));
+      expected.add(new LinePointers(3323, List.of(25, 38)));
+      expected.add(new LinePointers(4058, List.of(687)));
+      expected.add(new LinePointers(12310, List.of(67)));
+      expected.add(new LinePointers(12655, List.of(1033)));
+      expected.add(new LinePointers(12772, List.of(507)));
+      expected.add(new LinePointers(13749, List.of(1459)));
+      expected.add(new LinePointers(16153, List.of(48, 216)));
+
+      // asserts
+      assertEquals(expected, actual);
+    } catch (Exception e) {
       fail();
     }
   }
@@ -96,42 +93,42 @@ class SubstringTest {
   public void nullInputStreamTest() {
 
     // actual
-    InputStream isc = getClass().getClassLoader().getResourceAsStream("f1.in");
+    try (InputStream isc = getClass().getClassLoader().getResourceAsStream("f1.in")) {
 
-    Substring fileSubstr = new Substring(isc);
-    Substring fileSubstring = new Substring(null);
+      Substring fileSubstr = new Substring(isc);
+      Substring fileSubstring = new Substring(null);
 
-    // asserts
-    assertNull(fileSubstring.algorithmRabinKarp("Hello"));
-    assertNull(fileSubstr.algorithmRabinKarp(""));
-    assertNull(fileSubstr.algorithmRabinKarp(null));
+      // asserts
+      assertNull(fileSubstring.algorithmRabinKarp("Hello"));
+      assertNull(fileSubstr.algorithmRabinKarp(""));
+      assertNull(fileSubstr.algorithmRabinKarp(null));
+    } catch (Exception e) {
+      fail();
+    }
   }
 
   @Test
   public void overStringTest() {
 
     // actual
-    InputStream fin = getClass().getClassLoader().getResourceAsStream("f3.in");
-    if (fin != null) {
-      try (Scanner isc = new Scanner(fin)) {
-        String textInputString = isc.nextLine();
-        InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
-        if (textStream == null) {
-          fail();
-        }
-        Substring fsub = new Substring(textStream);
-
-        List<LinePointers> actual;
-        String find = isc.nextLine();
-
-        actual = fsub.algorithmRabinKarp(find);
-
-        // asserts
-        assertNull(actual);
-      } catch (Exception e) {
+    try (InputStream fin = getClass().getClassLoader().getResourceAsStream("f3.in");
+         Scanner isc = new Scanner(Objects.requireNonNull(fin))
+    ) {
+      String textInputString = isc.nextLine();
+      InputStream textStream = getClass().getClassLoader().getResourceAsStream(textInputString);
+      if (textStream == null) {
         fail();
       }
-    } else {
+      Substring fsub = new Substring(textStream);
+
+      List<LinePointers> actual;
+      String find = isc.nextLine();
+
+      actual = fsub.algorithmRabinKarp(find);
+
+      // asserts
+      assertNull(actual);
+    } catch (Exception e) {
       fail();
     }
   }
@@ -139,12 +136,20 @@ class SubstringTest {
   @Test
   public void largeFileTest() {
     try (RandomAccessFile f = new RandomAccessFile("./src/test/resources/text.txt", "rw")) {
-      f.setLength(200000L);
-
+      f.setLength(20000000L);
+      f.seek(2500);
+      f.writeBytes("hereyouare");
+      f.seek(5000);
+      f.writeBytes("hereyouare");
+      f.seek(2576000);
+      f.writeBytes("hereyouare");
+      f.seek(0);
       Substring sub = new Substring(Channels.newInputStream(f.getChannel()));
-      List<LinePointers> actual = sub.algorithmRabinKarp("jjifjiweiweurihbfmnfskdfjiewjf");
+      List<LinePointers> actual = sub.algorithmRabinKarp("hereyouare");
 
-      assertTrue(actual.isEmpty());
+      List<LinePointers> expected = new ArrayList<>();
+      expected.add(new LinePointers(1, Arrays.asList(2500, 5000, 2576000)));
+      assertEquals(expected, actual);
     } catch (Exception c) {
       fail();
     }
