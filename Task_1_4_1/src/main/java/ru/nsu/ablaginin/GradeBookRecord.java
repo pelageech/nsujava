0a07fe60ba4f441c2014bc0ac7ee6aea6468f30b
pelageech <va.oleg2015@yandex.ru>
2022-11-18 20:46:00 +0700

    javadoc

--- Task_1_4_1/src/main/java/ru/nsu/ablaginin/GradeBookRecord.java
+++ Task_1_4_1/src/main/java/ru/nsu/ablaginin/GradeBookRecord.java
@@ -2,6 +2,14 @@ package ru.nsu.ablaginin;
 
 import java.util.Calendar;
 
+/**
+ * Record for the grade book.
+ *
+ * @param subject name of the subject
+ * @param grade grade the student has got
+ * @param teacher the teacher who gives a course
+ * @param date the date of grade
+ */
 public record GradeBookRecord(String subject, Integer grade, String teacher, Calendar date) {
 
 }
