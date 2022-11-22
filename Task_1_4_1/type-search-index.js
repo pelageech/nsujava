8c27cb2b9d708f8865c13379c519cbb7c018c724
pelageech <pelageech@users.noreply.github.com>
2022-11-22 09:27:16 +0000

    Deploying to gh-pages from @ pelageech/nsujava@f89759bafd3f3e271293751ba4b6debd3d283b6c 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ablaginin","l":"GradeBook"},{"p":"ru.nsu.ablaginin","l":"GradeBookRecord"}];updateSearchResults();
