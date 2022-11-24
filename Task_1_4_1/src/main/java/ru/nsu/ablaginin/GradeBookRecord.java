143493ee61b7c4ce1ab9a0dadb1356beed7ca650
pelageech <va.oleg2015@yandex.ru>
2022-11-24 12:04:42 +0700

    record constructor for notnull checking

--- Task_1_4_1/src/main/java/ru/nsu/ablaginin/GradeBookRecord.java
+++ Task_1_4_1/src/main/java/ru/nsu/ablaginin/GradeBookRecord.java
@@ -1,6 +1,7 @@
 package ru.nsu.ablaginin;
 
 import java.util.Calendar;
+import java.util.Optional;
 
 /**
  * Record for the grade book.
@@ -10,6 +11,10 @@ import java.util.Calendar;
  * @param teacher the teacher who gives a course
  * @param date the date of grade
  */
-public record GradeBookRecord(String subject, Integer grade, String teacher, Calendar date) {
-
+public record GradeBookRecord(String subject, Grade grade, Optional<String> teacher, Optional<Calendar> date) {
+  public GradeBookRecord{
+    if (subject == null || grade == null) {
+      throw new NullPointerException();
+    }
+  }
 }
