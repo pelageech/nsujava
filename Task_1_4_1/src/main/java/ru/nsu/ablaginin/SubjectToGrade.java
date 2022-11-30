c0595f8d8938142ab422d6364f0750f35011066d
pelageech <va.oleg2015@yandex.ru>
2022-11-30 08:38:00 +0700

    typedef changed

--- Task_1_4_1/src/main/java/ru/nsu/ablaginin/SubjectToGrade.java
+++ Task_1_4_1/src/main/java/ru/nsu/ablaginin/SubjectToGrade.java
@@ -1,10 +1,15 @@
 package ru.nsu.ablaginin;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Typedef of HashMap String, GradeBookRecord.
  */
-public class SubjectToGrade extends HashMap<String, GradeBookRecord> {
+public class SubjectToGrade {
+  private final Map<String, GradeBookRecord> map = new HashMap<>();
 
+  public Map<String, GradeBookRecord> getMap() {
+    return map;
+  }
 }
