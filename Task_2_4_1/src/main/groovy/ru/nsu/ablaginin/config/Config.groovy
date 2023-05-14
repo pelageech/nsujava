943bde28a080c770cf87bdf4c4fa5c471c59e5ed
pelageech <va.oleg2015@yandex.ru>
2023-05-14 21:28:49 +0700

    list of students are parsed

--- Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/config/Config.groovy
+++ Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/config/Config.groovy
@@ -1,37 +1,39 @@
 package ru.nsu.ablaginin.config
 
-student {
-    nickname = "fdfdf"
-    name = "kavo"
-    url = "fdfdf"
-    marks {
-        mark {
-            mark = 5
-            date = "11-05-2023"
+students {
+    student {
+        nickname = "fdfdf"
+        name = "kavo"
+        url = "fdfdf"
+        marks {
+            mark {
+                mark = 5
+                date = "11-05-2023"
+            }
+            mark {
+                mark = 2
+                date = "12-05-2023"
+            }
         }
-        mark {
-            mark = 2
-            date = "12-05-2023"
+        tasks {
+            task {
+                id = "task-1-1-1"
+                date = "11-09-2023"
+            }
+            task {
+                id = "task-1-2-1"
+                date = "11-09-2023"
+            }
         }
-    }
-    tasks {
-        task {
-            id = "task-1-1-1"
-            date = "11-09-2023"
+        classes {
+            newClass {
+                date = "11-05-2023"
+                attendance = true
+            }
         }
-        task {
-            id = "task-1-2-1"
-            date = "11-09-2023"
+        group {
+            number = 12
         }
-    }
-    classes {
-        newClass {
-            date = "11-05-2023"
-            attendance = true
-        }
-    }
-    group {
-        number = 12
     }
 }
 tasks {
--- Task_2_4_1/src/main/java/ru/nsu/ablaginin/dsl/DSL.java
+++ Task_2_4_1/src/main/java/ru/nsu/ablaginin/dsl/DSL.java
@@ -2,19 +2,29 @@ package ru.nsu.ablaginin.dsl;
 
 import groovy.lang.Closure;
 import lombok.Data;
+import ru.nsu.ablaginin.dsl.bricks.Group;
 import ru.nsu.ablaginin.dsl.bricks.Student;
+import ru.nsu.ablaginin.dsl.bricks.lists.StudentList;
 import ru.nsu.ablaginin.dsl.bricks.lists.TaskList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class DSL {
-    private Student student;
+    private StudentList students = new StudentList();
+    private Map<Group, Student> studentsByGroup = new HashMap<>();
     private TaskList taskList;
 
-    public void student(Closure c) {
-        student = new Student();
-        c.setDelegate(student);
+    public void students(Closure c) {
+        c.setDelegate(students);
         c.setResolveStrategy(Closure.DELEGATE_ONLY);
         c.call();
+        for (var s : students.getStudentList()) {
+            studentsByGroup.put(s.getGroup(), s);
+        }
     }
 
     public void tasks(Closure c) {
