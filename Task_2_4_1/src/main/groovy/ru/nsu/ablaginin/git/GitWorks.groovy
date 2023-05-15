32f8d8e234372a8d0dbd8df26625b1abcc70212f
pelageech <va.oleg2015@yandex.ru>
2023-05-15 12:03:10 +0700

    init and open

--- Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/git/GitWorks.groovy
+++ Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/git/GitWorks.groovy
@@ -3,25 +3,35 @@ package ru.nsu.ablaginin.git
 import lombok.Cleanup
 import lombok.SneakyThrows
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.api.errors.JGitInternalException
-import org.eclipse.jgit.api.errors.RefNotFoundException
-import org.eclipse.jgit.revwalk.filter.CommitTimeRevFilter
-import org.eclipse.jgit.transport.URIish
-import ru.nsu.ablaginin.Compiler
-import ru.nsu.ablaginin.dsl.bricks.Student
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
 import java.nio.file.Files
+import java.nio.file.Paths
+
 import java.nio.file.Path
 
 class GitWorks {
 
+    static Git init(String name) {
+        Path repoPath = Paths.get("./repos/"+name);
+
+        return Git.init().setDirectory(repoPath.toFile()).call()
+    }
+
+    static Git open(String name) {
+        Path repoPath = Paths.get("./repos/"+name);
+
+        return Git.open(repoPath.toFile())
+    }
+
     @SneakyThrows
-    void pull(String repo, String branch) {
+    void pull(String name, String branch) {
 
     }
 
     @SneakyThrows
-    void clone(String repo) {
+    void clone(String name) {
 
     }
 
