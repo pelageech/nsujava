8d2d7654d9557d43663b5c7202813c3920419355
pelageech <pelageech@users.noreply.github.com>
2023-05-16 06:14:03 +0000

    Deploying to gh-pages from @ pelageech/nsujava@6f07978dbc575c0e66a389bc588c771fa489ca79 🚀

--- Task_2_4_1/package-search-index.js
+++ Task_2_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.ablaginin.dsl"},{"l":"ru.nsu.ablaginin.dsl.bricks"},{"l":"ru.nsu.ablaginin.dsl.bricks.lists"},{"l":"ru.nsu.ablaginin.helper"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.ablaginin.builder"},{"l":"ru.nsu.ablaginin.dsl"},{"l":"ru.nsu.ablaginin.dsl.bricks"},{"l":"ru.nsu.ablaginin.dsl.bricks.lists"},{"l":"ru.nsu.ablaginin.git"},{"l":"ru.nsu.ablaginin.helper"}];updateSearchResults();
