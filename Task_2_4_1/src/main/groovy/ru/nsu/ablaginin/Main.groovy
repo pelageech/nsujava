255947b831ce7adfd6755ab0554bb8c7beb61b67
pelageech <va.oleg2015@yandex.ru>
2023-05-17 11:58:01 +0700

    checkstyleMain doesn't work

--- Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/Main.groovy
+++ Task_2_4_1/src/main/groovy/ru/nsu/ablaginin/Main.groovy
@@ -7,25 +7,35 @@ import ru.nsu.ablaginin.dsl.bricks.Student
 import ru.nsu.ablaginin.git.GitWorks
 import ru.nsu.ablaginin.helper.FileUtils
 
+b = !true
+
+if (b) {
 // create a new student
-student = Compiler.compile(
-        new File(getClass().getClassLoader().getResource("./conf/artyom.groovy").toURI()), Student.class
-) as Student
+    var student = Compiler.compile(
+            new File(getClass().getClassLoader().getResource("./conf/artyom.groovy").toURI()), Student.class
+    ) as Student
 
 // create a temp repo dir
-f = File.createTempDir("temp-repo")
-f.deleteOnExit()
+    var f = File.createTempDir("temp-repo")
+    f.deleteOnExit()
 
 // clone there
-GitWorks.clone(student.url, f, Optional.of("task-1-1-1"))
+    GitWorks.clone(student.url, f, Optional.of("task-1-2-3"))
 
 // test the project
-path = f.path.concat("\\Task_1_1_1")
-try {
-    c = Builder.buildTest(new File(path))
-    println c
-} catch (Exception e) {
-    println e.getMessage()
+    var path = f.path.concat("/Task_1_2_3")
+    try {
+        Builder.jacocoTestReport(new File(path))
+    } catch (Exception e) {
+        println e.getMessage()
+    }
+} else {
+    try {
+        Builder.checkCodeStyle(new File(
+                "C:\\Users\\Mi\\AppData\\Local\\Temp\\temp-repo12926346480599657874\\Task_1_2_3"
+        ))
+    } catch (Exception e) {
+        println e.getMessage()
+    }
 }
-
-FileUtils.deleteRecursively(f)
+// FileUtils.deleteRecursively(f)
--- Task_2_4_1/src/main/java/ru/nsu/ablaginin/builder/Builder.java
+++ Task_2_4_1/src/main/java/ru/nsu/ablaginin/builder/Builder.java
@@ -1,28 +1,50 @@
 package ru.nsu.ablaginin.builder;
 
 import lombok.SneakyThrows;
-import org.gradle.tooling.BuildException;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.model.GradleTask;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
 
 public class Builder {
+
     @SneakyThrows
-    public static boolean buildTest(File projectDir) {
+    public static void build(File projectDir, String task) {
         if (!projectDir.exists()) {
             throw new FileNotFoundException("file " + projectDir.getPath() + " not found");
         }
 
         try (ProjectConnection conn = GradleConnector.newConnector().forProjectDirectory(projectDir).connect()) {
-            conn.newBuild().forTasks("test").run();
-        } catch (BuildException e) {
+            conn.newBuild().forTasks(task).run();
+        }
+    }
+
+    public static boolean buildTest(File projectDir) {
+        try {
+            build(projectDir, "test");
+        } catch (Exception e) {
             System.out.println("Failed to build");
             System.out.println(e.getMessage());
             return false;
         }
-
         return true;
+    }
+
+    @SneakyThrows
+    public static void checkCodeStyle(File projectDir) {
+        build(projectDir, "checkstyleMain");
+
+        var directory = new File("./reports/$name/$lab/codeCoverage");
+        var dir = Path.of("./reports/$name/$lab/codeCoverage");
+        var origin = Path.of("./repos/$name/$lab/build/jacocoHtml");
+
+    }
+
+    @SneakyThrows
+    public static void jacocoTestReport(File projectDir) {
+        build(projectDir, "jacocoTestReport");
     }
 }
